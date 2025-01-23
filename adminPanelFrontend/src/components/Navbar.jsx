1da972878e62557bafd18dcd32eae4070e8d1dee
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2025-01-23 23:57:20 +0300

    update

--- adminPanelFrontend/src/components/Navbar.jsx
+++ adminPanelFrontend/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+import { MessageSquare, User } from 'react-feather'
+
 export default function Navbar() {
 
     return (
         <div className='nav'>
-            <div >
-                navbar
-            </div>
+            <div className='nav-title'><h2>Admin Dashboard</h2></div>
+            <ul className='nav-list'>
+                <li className="nav-list-item"><input type="search" name="" id="" /></li>
+                <li className="nav-list-item"><MessageSquare /></li>
+                <li className="nav-list-item"><User /></li>
+            </ul>
         </div>
     )
 }
--- adminPanelFrontend/src/pages/Homepage.jsx
+++ adminPanelFrontend/src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-import DashboardCard from "../components/DashboardCard";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -8,13 +7,11 @@ export default function Homepage() {
             <div class="home-dashboard">
                 <div class="div1"><Sidebar /></div>
                 <div class="div2"><Navbar /></div>
-                <div class="div3"><DashboardCard /></div>
-                <div class="div4"><DashboardCard /></div>
-                <div class="div5"> <DashboardCard /></div>
-                <div class="div6"> <DashboardCard /></div>
+                <div class="div3"></div>
+                <div class="div4"></div>
+                <div class="div5"> </div>
+                <div class="div6"> </div>
             </div>
-            
-            
         </>
     )
 }
